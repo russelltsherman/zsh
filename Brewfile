2ecd6590d4a27987f684baa966f6ac8a27a86890
Russell <russell.t.sherman@gmail.com>
2020-12-05 12:49:05 -0600

    add antibody zsh plugin manager

---
+++ Brewfile
@@ -0,0 +1,3 @@
+# The fastest shell plugin manager
+# http://getantibody.github.io
+brew "getantibody/tap/antibody"
---
+++ zsh.d/antibody.zsh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+
+# https://github.com/unixorn/awesome-zsh-plugins#antibody
+
+# if antibody command is installed.. initialize it
+[ $commands[antibody] ] && source <(antibody init)
