65a9ae6a5c014c0720d9cf579751c48321504d96
Russell <russell.t.sherman@gmail.com>
2020-12-10 09:11:00 -0600

    add kpoof

---
+++ zsh.d/kpoof.zsh
@@ -0,0 +1,14 @@
+export KPOOF_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.kpoof" || printf %s "${XDG_CONFIG_HOME}/kpoof")"
+
+if chk::command "kpoof"
+then
+
+else
+  echo "kpoof not found. execute 'kpoof::install' to install it."
+
+  kpoof::install() {
+    git clone https://https://github.com/farmotive/kpoof $KPOOF_DIR
+    ln -s $KPOOF_DIR/bin/* ~/bin
+  }
+fi
+
