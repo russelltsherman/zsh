32407f5ddbcb0c0b160ab3c126c52bb4624d2976
Russell <russell.t.sherman@gmail.com>
2020-12-10 07:23:07 -0600

    add draft

---
+++ zsh.d/draft.zsh
@@ -0,0 +1,28 @@
+if [[ $commands[draft] ]]
+then
+
+else
+    echo "draft not found. execute 'draft::install' to install it."
+
+    draft::install() {
+      version="0.16.0"
+
+      if chk::osx
+      then
+        wget https://azuredraft.blob.core.windows.net/draft/draft-v$version-darwin-amd64.tar.gz
+        tar -xvf draft-v$version-darwin-amd64.tar.gz
+        mv darwin-amd64/draft ~/bin
+        rm -rf darwin-amd64
+        rm draft-v$version-darwin-amd64.tar.gz
+      fi
+      
+      if chk::ubuntu
+      then
+        wget https://azuredraft.blob.core.windows.net/draft/draft-v$version-linux-amd64.tar.gz
+        tar -xvf draft-v$version-linux-amd64.tar.gz
+        mv linux-amd64/draft ~/bin
+        rm -rf linux-amd64
+        rm draft-v$version-linux-amd64.tar.gz
+      fi
+    }
+fi
