69eba820d1316ca3e0b593611451d95405f2626c
Russell <russell.t.sherman@gmail.com>
2020-12-10 07:22:59 -0600

    fmt

--- zsh.d/gibo.zsh
+++ zsh.d/gibo.zsh
@@ -2,15 +2,15 @@ if [[ $commands[gibo] ]]
 then
 
 else
-    echo "gibo not found. execute 'gibo::install' to install it."
+  echo "gibo not found. execute 'gibo::install' to install it."
 
-    gibo::install() {
-        chk::osx && pkg::install::brew "gibo"
+  gibo::install() {
+    chk::osx && pkg::install::brew "gibo"
 
-        if chk::linux
-        then
-          git clone https://github.com/simonwhitaker/gibo.git ~/bin/gibo
-        fi
-    }
+    if chk::linux
+    then
+      git clone https://github.com/simonwhitaker/gibo.git ~/bin/gibo
+    fi
+  }
 fi
 
