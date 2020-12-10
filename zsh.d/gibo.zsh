fb5126f85f7267e55fad55cd2b8f55731f2a8d7e
Russell <russell.t.sherman@gmail.com>
2020-12-10 06:38:23 -0600

    add git ignore boilerplate

---
+++ zsh.d/gibo.zsh
@@ -0,0 +1,16 @@
+if [[ $commands[gibo] ]]
+then
+
+else
+    echo "gibo not found. execute 'gibo::install' to install it."
+
+    gibo::install() {
+        chk::osx && pkg::install::brew "gibo"
+
+        if chk::linux
+        then
+          git clone https://github.com/simonwhitaker/gibo.git ~/bin/gibo
+        fi
+    }
+fi
+
