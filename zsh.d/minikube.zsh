6ac653c3b7c8e9534012ab51afe3f4ff367ed24c
Russell <russell.t.sherman@gmail.com>
2020-12-10 06:39:13 -0600

    add minikube

---
+++ zsh.d/minikube.zsh
@@ -0,0 +1,16 @@
+if [[ $commands[minikube] ]]
+then
+
+else
+    echo "minikube not found. execute 'minikube::install' to install it."
+
+    minikube::install() {
+        chk::osx && pkg::install::brew "minikube"
+        
+        if chk::ubuntu
+        then
+          curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+          sudo dpkg -i minikube_latest_amd64.deb
+        fi
+    }
+fi
