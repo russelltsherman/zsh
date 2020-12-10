c1f89f66cd6ac19691fc07cad9ba0c73e5ee8569
Russell Sherman <russell.sherman@getupside.com>
2020-12-10 11:12:24 -0600

    consistently use tar

--- zsh.d/krew.zsh
+++ zsh.d/krew.zsh
@@ -12,7 +12,7 @@ else
 
     krew::install() {
       wget "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.tar.gz"
-      tar zxvf krew.tar.gz
+      tar -zxvf krew.tar.gz
 
       KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_$(uname -m | sed -e 's/x86_64/amd64/' -e 's/arm.*$/arm/')"
       "$KREW" install krew
