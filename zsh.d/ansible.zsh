f0b626c1e8492ab93fd2912d88e20dfd3e49f266
Russell <russell.t.sherman@gmail.com>
2020-12-10 06:38:05 -0600

    add ansible

---
+++ zsh.d/ansible.zsh
@@ -0,0 +1,30 @@
+
+if [[ $commands[ansible] ]]
+then
+    alias a='ansible'
+    alias ap='ansible-playbook'
+    alias al='ansible-pull'
+    alias avc='ansible-vault create'
+    alias ave='ansible-vault edit'
+    alias avr='ansible-vault rekey'
+    alias avenc='ansible-vault encrypt'
+    alias avdec='ansible-vault decrypt'
+    alias avv='ansible-vault view'
+    alias aga='ansible-galaxy'
+    alias agad='ansible-galaxy delete'
+    alias agai='ansible-galaxy install'
+    alias agaim='ansible-galaxy import'
+    alias againf='ansible-galaxy info'
+    alias agal='ansible-galaxy list'
+    alias agalog='ansible-galaxy login'
+    alias agar='ansible-galaxy remove'
+    alias agas='ansible-galaxy search'
+
+else
+    echo "ansible not found. execute 'ansible::install' to install it."
+
+    ansible::install() {
+      pkg::install "ansible" "ppa:ansible/ansible"
+    }
+fi
+
