768790703369d06a0b5a3a3f99f8ab7ac8b18b18
Russell <russell.t.sherman@gmail.com>
2020-12-11 08:52:26 -0600

    add recompile functiion

--- zsh.d/_zsh.zsh
+++ zsh.d/_zsh.zsh
@@ -1,3 +1,23 @@
+# zsh::recompile 
+# usage:
+#     zsh::recompile 
+zsh::recompile() {
+  autoload -U zrecompile
+  rm -f "${ZDOTDIR}/*.zwc"
+  [ -f "${ZDOTDIR}/.zshrc" ] && zrecompile -p "${ZDOTDIR}/.zshrc"
+  [ -f "${ZDOTDIR}/.zshrc.zwc.old" ] && rm -f "${ZDOTDIR}/.zshrc.zwc.old"
+
+  for f in "${ZDOTDIR}"/**/*.zsh; do
+    [ -f "$f" ] && zrecompile -p "$f"
+    [ -f "${f}.zwc.old" ] && rm -f "${f}.zwc.old"
+  done
+
+  [ -f "${ZDOTDIR}/.zcompdump" ] && zrecompile -p "${ZDOTDIR}/.zcompdump"
+  [ -f "${ZDOTDIR}/.zcompdump.zwc.old" ] && rm -f "${ZDOTDIR}/.zcompdump.zwc.old"
+
+  . "${ZDOTDIR}/.zshrc"
+}
+
 # zsh::reload reload shell configuration
 # usage:
 #     zsh::reload 
