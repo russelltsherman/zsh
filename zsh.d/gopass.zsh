98265c9c328bff0a753b3bf9decdfe3af92698e2
Russell <russell.t.sherman@gmail.com>
2020-12-10 20:38:00 -0600

    consistent go get

--- zsh.d/gopass.zsh
+++ zsh.d/gopass.zsh
@@ -5,6 +5,6 @@ else
     echo "gopass not found. execute 'gopass::install' to install it."
 
     gopass::install() {
-      GO111MODULE=on go get github.com/gopasspw/gopass
+      go get github.com/gopasspw/gopass
     }
 fi
