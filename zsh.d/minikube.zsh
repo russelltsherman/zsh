67c99e21e0e23e9bcebceb2ce101b9af644cbd58
Russell <russell.t.sherman@gmail.com>
2020-12-10 21:27:06 -0600

    fmt

--- zsh.d/minikube.zsh
+++ zsh.d/minikube.zsh
@@ -2,15 +2,15 @@ if chk::command "minikube"
 then
 
 else
-    echo "minikube not found. execute 'minikube::install' to install it."
+  echo "minikube not found. execute 'minikube::install' to install it."
 
-    minikube::install() {
-        chk::osx && pkg::install::brew "minikube"
-        
-        if chk::ubuntu
-        then
-          curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
-          sudo dpkg -i minikube_latest_amd64.deb
-        fi
-    }
+  minikube::install() {
+    chk::osx && pkg::install::brew "minikube"
+    
+    if chk::ubuntu
+    then
+      curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+      sudo dpkg -i minikube_latest_amd64.deb
+    fi
+  }
 fi
