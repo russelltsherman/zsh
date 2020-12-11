c4b9a85e3b8dbc48c5a3bfb67edc12b92787cf20
Russell <russell.t.sherman@gmail.com>
2020-12-10 22:11:01 -0600

    correct eksctl checks

--- zsh.d/eksctl.zsh
+++ zsh.d/eksctl.zsh
@@ -1,10 +1,10 @@
-if chk::command "draft"
+if chk::command "eksctl"
 then
   eval $(eksctl completion $SHELL)
 else
-  echo "draft not found. execute 'draft::install' to install it."
+  echo "eksctl not found. execute 'eksctl::install' to install it."
 
-  draft::install() {
+  eksctl::install() {
     wget "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz"
     tar -xvf eksctl_$(uname -s)_amd64.tar.gz
     mv eksctl ~/bin
