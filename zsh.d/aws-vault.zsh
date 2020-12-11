129cc95b77c45e1e2fd2f080b2e85496680dc15a
Russell <russell.t.sherman@gmail.com>
2020-12-10 22:10:45 -0600

    comment functions due to alias conflict

--- zsh.d/aws-vault.zsh
+++ zsh.d/aws-vault.zsh
@@ -18,112 +18,112 @@ then
   #--------------------------------------------------------------------#
   # Convenience Functions                                              #
   #--------------------------------------------------------------------#
-  avl() {
-    aws-vault login $@
-  }
+  # avl() {
+  #   aws-vault login $@
+  # }
 
-  avll() {
-    aws-vault login -s $@
-  }
+  # avll() {
+  #   aws-vault login -s $@
+  # }
 
-  ave() {
-    aws-vault exec $@
-  }
+  # ave() {
+  #   aws-vault exec $@
+  # }
 
-  avsh() {
-    case ${AWS_VAULT_PL_MFA} in
-      inline)
-        aws-vault exec -t $2 $1 -- zsh
-        ;;
-      yubikey)
-        totp=${2:-$1}
-        aws-vault exec -t $(ykman oath code --single $totp) $1 -- zsh
-        ;;
-      *)
-        aws-vault exec $1 -- zsh
-        ;;
-    esac
-  }
+  # avsh() {
+  #   case ${AWS_VAULT_PL_MFA} in
+  #     inline)
+  #       aws-vault exec -t $2 $1 -- zsh
+  #       ;;
+  #     yubikey)
+  #       totp=${2:-$1}
+  #       aws-vault exec -t $(ykman oath code --single $totp) $1 -- zsh
+  #       ;;
+  #     *)
+  #       aws-vault exec $1 -- zsh
+  #       ;;
+  #   esac
+  # }
 
-  avli() {
-    local login_url
-    case ${AWS_VAULT_PL_MFA} in
-      inline)
-        login_url="$(avll -t $2 $1)"
-        ;;
-      yubikey)
-        totp=${2:-$1}
-        login_url="$(avll -t $(ykman oath code --single $totp) $1)"
-        ;;
-      *)
-        login_url="$(avll $1)"
-        ;;
-    esac
+  # avli() {
+  #   local login_url
+  #   case ${AWS_VAULT_PL_MFA} in
+  #     inline)
+  #       login_url="$(avll -t $2 $1)"
+  #       ;;
+  #     yubikey)
+  #       totp=${2:-$1}
+  #       login_url="$(avll -t $(ykman oath code --single $totp) $1)"
+  #       ;;
+  #     *)
+  #       login_url="$(avll $1)"
+  #       ;;
+  #   esac
 
-    if [ $? -ne 0 ]; then
-      echo "Could not login" >&2
-      return 1
-    fi
+  #   if [ $? -ne 0 ]; then
+  #     echo "Could not login" >&2
+  #     return 1
+  #   fi
 
-    local browser="$(_find_browser)"
+  #   local browser="$(_find_browser)"
 
-    if chk::osx ; then
-      case $browser in
-        org.mozilla.firefox)
-          # Ensure a profile is created (can run idempotently) and launch it as a disowned process
-          /Applications/Firefox.app/Contents/MacOS/firefox --CreateProfile $1 2>/dev/null && \
-          /Applications/Firefox.app/Contents/MacOS/firefox --no-remote -P $1 "${login_url}" 2>/dev/null &!
-          ;;
-        org.mozilla.firefoxdeveloperedition)
-          /Applications/Firefox\ Developer\ Edition.app/Contents/MacOS/firefox --CreateProfile $1 2>/dev/null && \
-          /Applications/Firefox\ Developer\ Edition.app/Contents/MacOS/firefox --no-remote -P $1 "${login_url}" 2>/dev/null &!
-          ;;
-        com.google.chrome)
-          echo "${login_url}" | xargs -t nohup /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome %U --no-first-run --new-window --disk-cache-dir=$(mktemp -d /tmp/chrome.XXXXXX) --user-data-dir=$(mktemp -d /tmp/chrome.XXXXXX) > /dev/null 2>&1 &
-          ;;
-        com.brave.Browser)
-          echo "${login_url}" | xargs -t nohup /Applications/Brave\ Browser.app/Contents/MacOS/Brave\ Browser %U --no-first-run --new-window --disk-cache-dir=$(mktemp -d /tmp/brave.XXXXXX) --user-data-dir=$(mktemp -d /tmp/brave.XXXXXX) > /dev/null 2>&1 &
-          ;;
-        *)
-          # NOTE PRs welcome to add your browser
-          echo "Sorry, I don't know how to launch your default browser ($browser) :-("
-          ;;
-      esac
-    elif chk::linux ; then
-      case $browser in
-        google-chrome)
-          echo "${login_url}" | xargs -t nohup google-chrome %U --no-first-run --new-window --start-maximized --disk-cache-dir=$(mktemp -d /tmp/chrome.XXXXXX) --user-data-dir=$(mktemp -d /tmp/chrome.XXXXXX) > /dev/null 2>&1 &
-          ;;
-        brave-browser)
-          echo "${login_url}" | xargs -t nohup brave-browser %U --no-first-run --new-window --start-maximized --disk-cache-dir=$(mktemp -d /tmp/brave.XXXXXX) --user-data-dir=$(mktemp -d /tmp/brave.XXXXXX) > /dev/null 2>&1 &
-          ;;
-        *)
-          # NOTE PRs welcome to add your browser
-          echo "Sorry, I don't know how to launch your default browser ($browser) :-("
-          ;;
-      esac
-    else
-      # NOTE this is untested - PRs welcome to improve it.
-      echo "${login_url}" | xargs xdg-open
-    fi
-  }
+  #   if chk::osx ; then
+  #     case $browser in
+  #       org.mozilla.firefox)
+  #         # Ensure a profile is created (can run idempotently) and launch it as a disowned process
+  #         /Applications/Firefox.app/Contents/MacOS/firefox --CreateProfile $1 2>/dev/null && \
+  #         /Applications/Firefox.app/Contents/MacOS/firefox --no-remote -P $1 "${login_url}" 2>/dev/null &!
+  #         ;;
+  #       org.mozilla.firefoxdeveloperedition)
+  #         /Applications/Firefox\ Developer\ Edition.app/Contents/MacOS/firefox --CreateProfile $1 2>/dev/null && \
+  #         /Applications/Firefox\ Developer\ Edition.app/Contents/MacOS/firefox --no-remote -P $1 "${login_url}" 2>/dev/null &!
+  #         ;;
+  #       com.google.chrome)
+  #         echo "${login_url}" | xargs -t nohup /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome %U --no-first-run --new-window --disk-cache-dir=$(mktemp -d /tmp/chrome.XXXXXX) --user-data-dir=$(mktemp -d /tmp/chrome.XXXXXX) > /dev/null 2>&1 &
+  #         ;;
+  #       com.brave.Browser)
+  #         echo "${login_url}" | xargs -t nohup /Applications/Brave\ Browser.app/Contents/MacOS/Brave\ Browser %U --no-first-run --new-window --disk-cache-dir=$(mktemp -d /tmp/brave.XXXXXX) --user-data-dir=$(mktemp -d /tmp/brave.XXXXXX) > /dev/null 2>&1 &
+  #         ;;
+  #       *)
+  #         # NOTE PRs welcome to add your browser
+  #         echo "Sorry, I don't know how to launch your default browser ($browser) :-("
+  #         ;;
+  #     esac
+  #   elif chk::linux ; then
+  #     case $browser in
+  #       google-chrome)
+  #         echo "${login_url}" | xargs -t nohup google-chrome %U --no-first-run --new-window --start-maximized --disk-cache-dir=$(mktemp -d /tmp/chrome.XXXXXX) --user-data-dir=$(mktemp -d /tmp/chrome.XXXXXX) > /dev/null 2>&1 &
+  #         ;;
+  #       brave-browser)
+  #         echo "${login_url}" | xargs -t nohup brave-browser %U --no-first-run --new-window --start-maximized --disk-cache-dir=$(mktemp -d /tmp/brave.XXXXXX) --user-data-dir=$(mktemp -d /tmp/brave.XXXXXX) > /dev/null 2>&1 &
+  #         ;;
+  #       *)
+  #         # NOTE PRs welcome to add your browser
+  #         echo "Sorry, I don't know how to launch your default browser ($browser) :-("
+  #         ;;
+  #     esac
+  #   else
+  #     # NOTE this is untested - PRs welcome to improve it.
+  #     echo "${login_url}" | xargs xdg-open
+  #   fi
+  # }
 
-  avp() {
-    local -a profiles
-    local _profile_text _role
-    if egrep -arn "^\[default\]" ~/.aws/config >/dev/null; then
-      profiles+="default: IAM_Keys"
-    fi
-    for item in $(grep "\[profile " ~/.aws/config | sed -e 's/.*profile \([a-zA-Z0-9_-]*\).*/\1/' | sort); do
-      _profile_text="$item: "
-      _role=$(aws --profile $item configure get role_arn)
-      if [ "$_role" != "" ]; then
-          _profile_text+="ROLE($_role) "
-      fi
-      profiles+=$_profile_text
-    done
-    printf '%s\n' "${profiles[@]}" | column -t
-  }
+  # avp() {
+  #   local -a profiles
+  #   local _profile_text _role
+  #   if egrep -arn "^\[default\]" ~/.aws/config >/dev/null; then
+  #     profiles+="default: IAM_Keys"
+  #   fi
+  #   for item in $(grep "\[profile " ~/.aws/config | sed -e 's/.*profile \([a-zA-Z0-9_-]*\).*/\1/' | sort); do
+  #     _profile_text="$item: "
+  #     _role=$(aws --profile $item configure get role_arn)
+  #     if [ "$_role" != "" ]; then
+  #         _profile_text+="ROLE($_role) "
+  #     fi
+  #     profiles+=$_profile_text
+  #   done
+  #   printf '%s\n' "${profiles[@]}" | column -t
+  # }
 
   #--------------------------------------------------------------------#
   # Utility Functions                                                  #
