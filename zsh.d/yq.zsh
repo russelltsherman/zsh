a2396af5d215c20e0a2a77e81d89fcdae7162cf0
Russell <russell.t.sherman@gmail.com>
2020-12-12 11:14:36 -0600

    go get yq

--- zsh.d/yq.zsh
+++ zsh.d/yq.zsh
@@ -6,13 +6,6 @@ else
   echo "yq not found. execute 'yq::install' to install it."
 
   yq::install() {
-    local version="3.4.1"
-    local platform
-
-    chk::osx && platform="darwin"
-    chk::linux && platform="linux"
-
-    wget "https://github.com/mikefarah/yq/releases/download/${version}/yq_${platform}_amd64" -O ~/bin/yq
-    chmod +x ~/bin/yq
+    GO111MODULE=on go get github.com/mikefarah/yq/v3
   }
 fi
