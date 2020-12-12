7a3e5728ccdc21e4a605c50f7cd2442619659878
Russell <russell.t.sherman@gmail.com>
2020-12-12 13:00:22 -0600

    add google chrome history search function

--- zsh.d/google-chrome.zsh
+++ zsh.d/google-chrome.zsh
@@ -1,6 +1,20 @@
 if chk::command "google-chrome"
 then
 
+  # gch - browse chrome history
+  gch() {
+    cols=$(( COLUMNS / 3 ))
+    sep='{::}'
+
+    cp ~/Library/Application\ Support/Google/Chrome/Default/History /tmp/gch 
+    
+    sqlite3 -separator $sep /tmp/gch \
+      "select substr(title, 1, $cols), url
+      from urls order by last_visit_time desc" |
+    awk -F $sep '{printf "%-'$cols's  \x1b[36m%s\x1b[m\n", $1, $2}' |
+    fzf --ansi --multi | sed 's#.*\(https*://\)#\1#' | xargs open
+  }
+
 else
   echo "google-chrome not found. execute 'google-chrome::install' to install it."
 
