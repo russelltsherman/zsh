8e96f2e10b1ca0212e78cf7fcb1a05285744c9fc
Russell <russell.t.sherman@gmail.com>
2020-12-14 05:37:55 -0600

    install direnv using asdf

---
+++ zsh.d/direnv.zsh
@@ -0,0 +1,28 @@
+
+if chk::command "direnv"
+then
+  # Hook direnv into your shell.
+  eval "$(asdf exec direnv hook zsh)"
+
+  # A shortcut for asdf managed direnv.
+  direnv() { 
+    asdf exec direnv "$@"
+  }
+
+else
+  echo "direnv not found. execute 'direnv::install' to install it."
+
+  direnv::install() {
+    asdf plugin-add direnv
+    asdf install direnv 2.20.0
+    asdf global direnv 2.20.0
+
+    cat > ~/.config/direnv/direnvrc <<- EOM
+# makes the use asdf feature available
+source "$(asdf direnv hook asdf)"
+
+# Uncomment the following line to make direnv silent by default.
+# export DIRENV_LOG_FORMAT=""
+EOM
+  }
+fi
