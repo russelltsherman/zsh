7630fb9ff3a994076eb925a5abe1fdd001d85fe5
Russell <russell.t.sherman@gmail.com>
2020-12-19 14:16:34 -0600

    asdf manage stern

--- zsh.d/stern.zsh
+++ zsh.d/stern.zsh
@@ -1,10 +1,15 @@
-if chk::command "stern"
+
+if chk::asdf::plugin 'stern'
 then
   ;
 else
   echo "stern not found. execute 'stern::install' to install it."
 
   stern::install() {
-    go get github.com/stern/stern
+    asdf plugin-add stern https://github.com/looztra/asdf-stern
+
+    asdf install stern $(asdf list-all stern | tail -1)
+    asdf global stern $(asdf list stern)
   }
+
 fi
