47cb91bb4f93d5a5dfb105f6caa3b4cce511231d
Russell <russell.t.sherman@gmail.com>
2020-12-19 14:17:13 -0600

    asdf manage yq

--- zsh.d/yq.zsh
+++ zsh.d/yq.zsh
@@ -1,11 +1,15 @@
 
-if chk::command "yq"
+if chk::asdf::plugin 'yq'
 then
   ;
 else
   echo "yq not found. execute 'yq::install' to install it."
 
   yq::install() {
-    GO111MODULE=on go get github.com/mikefarah/yq/v3
+    asdf plugin-add yq https://github.com/sudermanjr/asdf-yq.git
+
+    asdf install yq $(asdf list-all yq | tail -1)
+    asdf global yq $(asdf list yq)
   }
+
 fi
