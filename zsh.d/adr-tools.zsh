7d12a41f5961d9de8be05d592686f74e5cbe1e0d
Russell <russell.t.sherman@gmail.com>
2020-12-19 14:23:37 -0600

    asdf manage asd-tools

--- zsh.d/adr-tools.zsh
+++ zsh.d/adr-tools.zsh
@@ -1,13 +1,14 @@
-export ADR_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.adr" || printf %s "${XDG_CONFIG_HOME}/adr")"
 
-if chk::dir $ADR_DIR
+if chk::asdf::plugin 'adr-tools'
 then
-  export PATH="${PATH}:${ADR_DIR}/src"
-
+  ;
 else
-  echo "adr-tools not found. execute 'adr::install' to install it."
+  echo "adr-tools not found. execute 'adr-tools::install' to install it."
 
-  adr::install() {
-    git clone https://github.com/npryce/adr-tools $ADR_DIR
+  adr-tools::install() {
+    asdf plugin-add adr-tools https://gitlab.com/td7x/asdf/adr-tools.git
+
+    asdf install adr-tools 3.0.0
+    asdf global adr-tools $(asdf list adr-tools)
   }
 fi
