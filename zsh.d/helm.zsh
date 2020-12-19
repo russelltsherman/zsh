69068e59d3d5ef101908267f0a5c5c986634c864
Russell <russell.t.sherman@gmail.com>
2020-12-19 15:27:58 -0600

    asdf manage helm

--- zsh.d/helm.zsh
+++ zsh.d/helm.zsh
@@ -1,11 +1,14 @@
-if chk::command "helm"
-then
 
+if chk::asdf::plugin 'helm'
+then
+  ;
 else
   echo "helm not found. execute 'helm::install' to install it."
 
   helm::install() {
-    git clone https://github.com/helm/helm.git
-    make -C helm
+    asdf plugin-add helm https://github.com/Antiarchitect/asdf-helm.git
+
+    asdf install helm $(asdf list-all helm | tail -1) 
+    asdf global helm $(asdf list helm)
   }
 fi
