bdfe267edd3b8a53f16e1681493d03b3b40d0175
Russell <russell.t.sherman@gmail.com>
2020-12-19 13:12:20 -0600

    use asdf to manage nodejs

---
+++ zsh.d/nodejs.zsh
@@ -0,0 +1,23 @@
+
+if chk::asdf::plugin 'nodejs'
+then
+  ;
+else
+  echo "nodejs not found. execute 'nodejs::install' to install it."
+
+  nodejs::install() {
+    chk::osx && pkg::install::brew 'coreutils'
+    chk::osx && pkg::install::brew 'gpg'
+
+    chk::debian && pkg::install::debian 'curl dirmngr gpg'
+
+    asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+    bash -c '${ASDF_DATA_DIR}/plugins/nodejs/bin/import-release-team-keyring'
+    bash -c '${ASDF_DATA_DIR}/plugins/nodejs/bin/import-previous-release-team-keyring'
+
+    asdf install nodejs 14.15.1
+    asdf global nodejs $(asdf list nodejs)
+  }
+
+fi
+
