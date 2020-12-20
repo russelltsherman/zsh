890f43f0473fb79236e45c75de43af13a96f2899
Russell <russell.t.sherman@gmail.com>
2020-12-20 16:58:22 -0600

    chk::command

--- zsh.d/lazydocker.zsh
+++ zsh.d/lazydocker.zsh
@@ -2,6 +2,6 @@ if chk::command "lazydocker"
 then
   ;
 else
-  chk:command "go" && go get github.com/jesseduffield/lazydocker
+  chk::command "go" && go get github.com/jesseduffield/lazydocker
 fi
 
