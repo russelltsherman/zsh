b47f03735a7485f3015e90979af1c157a68f2beb
Russell <russell.t.sherman@gmail.com>
2020-12-20 16:37:48 -0600

    dropbox install

--- zsh.d/dropbox.zsh
+++ zsh.d/dropbox.zsh
@@ -1,4 +1,3 @@
-export DROPBOX_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.dropbox" || printf %s "${XDG_CONFIG_HOME}/dropbox")"
 
 if chk::app "dropbox"
 then
@@ -8,8 +7,8 @@ else
   
   if chk::linux
   then
-    ( cd $DROPBOX_DIR && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf - )
-    $DROPBOX_DIR/.dropbox-dist/dropboxd
+    ( cd ~/ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf - )
+    ~/.dropbox-dist/dropboxd
 
     wget https://www.dropbox.com/download?dl=packages/dropbox.py -O ~/bin/dropbox
   fi
