2b2e96e71ea36c4c993952f044778017f6e6a3ac
Russell <russell.t.sherman@gmail.com>
2021-04-17 18:05:28 -0500

    relocate github token file

--- zsh.d/github.zsh
+++ zsh.d/github.zsh
@@ -1,7 +1,7 @@
 
 if chk::asdf::plugin 'github-cli'
 then
-  export GITHUB_TOKEN_FILE="${GITHUB_TOKEN_FILE:-$XDG_CONFIG_HOME/.github_token}"
+  export GITHUB_TOKEN_FILE="${GITHUB_TOKEN_FILE:-$XDG_CONFIG_HOME/github/token}"
   export GITHUB_API_TOKEN="$(head -n 1 $GITHUB_TOKEN_FILE)"
   export GITHUB_PERSONAL_ACCESS_TOKEN=$GITHUB_API_TOKEN
 fi
