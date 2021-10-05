65b5c584c9ad753a88166f921973df1fc9b3fe5c
russell sherman <russell@paymentapproved.com>
2021-10-05 09:51:47 -0500

    load gitlab token if token file present

--- zsh.d/gitlab.zsh
+++ zsh.d/gitlab.zsh
@@ -1,7 +1,10 @@
 
-export GITLAB_TOKEN_FILE="${GITLAB_TOKEN_FILE:-$XDG_CONFIG_HOME/githab/token}"
+export GITLAB_TOKEN_FILE="${GITLAB_TOKEN_FILE:-$XDG_CONFIG_HOME/gitlab/token}"
 
-if [ -f $GITLAB_TOKEN_FILE ]
+if [ -f "$GITHUB_TOKEN_FILE" ]
 then
-  export GITLAB_API_TOKEN="$(head -n 1 $GITLAB_TOKEN_FILE)"
+    export GITLAB_TOKEN="$(head -n 1 $GITLAB_TOKEN_FILE)"
+    export GITLAB_URL="https://www.gitlab.com"
+    export GITLABBER_CLONE_METHOD="ssh"
 fi
+
