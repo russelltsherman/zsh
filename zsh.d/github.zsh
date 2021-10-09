179d15184cc223d1bdc2d4da3a10d067c4aa978f
russell sherman <russell.t.sherman@gmail.com>
2021-10-09 14:31:26 -0500

    if github token file exists export the token

--- zsh.d/github.zsh
+++ zsh.d/github.zsh
@@ -1,7 +1,8 @@
 
-if chk::asdf::plugin 'github-cli'
+export GITHUB_TOKEN_FILE="${GITHUB_TOKEN_FILE:-$XDG_CONFIG_HOME/github/token}"
+
+if [ -f $GITHUB_TOKEN_FILE ]
 then
-  export GITHUB_TOKEN_FILE="${GITHUB_TOKEN_FILE:-$XDG_CONFIG_HOME/github/token}"
   export GITHUB_API_TOKEN="$(head -n 1 $GITHUB_TOKEN_FILE)"
   export GITHUB_PERSONAL_ACCESS_TOKEN=$GITHUB_API_TOKEN
 fi
