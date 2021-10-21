7de3bdd829c08224778813ef9f5896d6ef4a2210
russell sherman <russell@paymentapproved.com>
2021-10-21 16:10:00 -0500

    add terraform aliases

--- zsh.d/terraform.zsh
+++ zsh.d/terraform.zsh
@@ -23,4 +23,24 @@ then
     impromptu::segment "$IMPROMPTU_TFENV_COLOR" \
       "${IMPROMPTU_TFENV_PREFIX}${IMPROMPTU_TFENV_SYMBOL}${version}${IMPROMPTU_TFENV_SUFFIX}"
   }
+
+  #--------------------------------------------------------------------#
+  # Aliases                                                            #
+  #--------------------------------------------------------------------#
+  alias tf='terraform'
+  alias tfa='terraform apply'
+  alias tfaa='terraform apply --auto-approve'
+  alias tfc='terraform console'
+  alias tfd='terraform destroy'
+  alias tfda='terraform destroy --auto-approve'
+  alias tff='terraform fmt'
+  alias tfi='terraform init'
+  alias tfge='terraform get'
+  alias tfgr='terraform graph'
+  alias tfo='terraform output'
+  alias tfpl='terraform plan'
+  alias tfpr='terraform providers'
+  alias tfs='terraform show'
+  alias tfva='terraform validate'
+  alias tfve='terraform version'
 fi
