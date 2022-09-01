0b75c1f8ea8fbefaeb3c0fa0d4024b08004286b5
russell sherman <russell.t.sherman@gmail.com>
2022-08-31 20:18:47 -0500

    add profile aliases

--- zsh.d/Ω_work.zsh
+++ zsh.d/Ω_work.zsh
@@ -1,5 +1,16 @@
 #!/usr/bin/env zsh
 
+if [ $ACTIVE_PROFILE = "blockhenge" ]
+then
+  alias::new 'cdco' 'cd ~/src/gitlab.com/bhco/cryptomnio && code .'
+  alias::new 'cdef' 'cd ~/src/gitlab.com/bhco/ef && code .'
+fi
+
+if [ $ACTIVE_PROFILE = "finbotsdev" ]
+then
+  alias::new 'cdfx' 'cd ~/src/github.com/finbotsdev/developer && code .'
+fi
+
 if [ $ACTIVE_PROFILE = "paymentapproved" ]
 then
   alias::new 'apps' 'cd ~/src/gitlab.com/paymentapproved/apps; code apps.code-workspace'
